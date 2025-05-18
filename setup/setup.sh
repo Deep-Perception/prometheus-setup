5fbef66f34babcca33ce322bbfdc9b81baad9594
Chris Clason <chris.clason@gmail.com>
2025-05-18 11:00:51 -0500

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -66,7 +66,7 @@ sudo usermod -aG docker $USER
 #Install Chrome
 #
 curl -O -z google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ./google-chrome-stable_current_amd64.deb -y
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
 
 #

7802a869df54f3fafddee829cd7ea7edbba3ffba
Chris Clason <chris.clason@gmail.com>
2025-05-18 10:55:43 -0500

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -66,7 +66,8 @@ sudo usermod -aG docker $USER
 #Install Chrome
 #
 curl -O -L google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+sudo dpkg -i ./google-chrome-stable_current_amd64.deb -y
+sudo apt-get install -f -y
 
 #
 #Install Hailo Driver
